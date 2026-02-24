8a7b8b3f8f03317a68a7ecb461ab6e6bb93ec1ff
FernandaTurcios <jenfer.turcios2005@gmail.com>
2026-02-24 10:22:41 -0600

    Por correr programa

--- obj/Debug/net8.0/Proyecto1Analizador.AssemblyInfo.cs
+++ obj/Debug/net8.0/Proyecto1Analizador.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto1Analizador")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d735f31a932d4732c975efcf569d9a3be5d9252a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6af93e27df6393bbf1c217cff1d2f510379f9d72")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto1Analizador")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto1Analizador")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
