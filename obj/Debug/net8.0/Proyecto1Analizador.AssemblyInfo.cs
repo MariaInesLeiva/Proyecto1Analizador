480291986baef5a121ed0860ccdfee2ff14a17b7
MariaInesLeiva <mileivaca@correo.url.edu.gt>
2026-02-25 20:12:01 -0600

    Archivos de prueba

--- obj/Debug/net8.0/Proyecto1Analizador.AssemblyInfo.cs
+++ obj/Debug/net8.0/Proyecto1Analizador.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto1Analizador")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb18597fe9d0e5bebc87aa61efbf59ece6546c11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc15e44b776fafd50eb26b2182f507d26262dc42")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto1Analizador")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto1Analizador")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
