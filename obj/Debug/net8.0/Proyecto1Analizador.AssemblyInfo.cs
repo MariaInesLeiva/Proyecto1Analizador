80529456d2fe3a1052047f6b99759940cfb9640b
MariaInesLeiva <mileivaca@correo.url.edu.gt>
2026-04-18 10:22:26 -0600

    cambios Minilang

--- obj/Debug/net8.0/Proyecto1Analizador.AssemblyInfo.cs
+++ obj/Debug/net8.0/Proyecto1Analizador.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto1Analizador")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c69c87876369968205950f152209a87c4e8dc3c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5141e1bc04a75749959e2871aa0ebda304a8adfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto1Analizador")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto1Analizador")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
